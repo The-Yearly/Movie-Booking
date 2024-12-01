ac32e7af48659a91cb6f07c0edc06fd27c1e50d3
Abhinav Dileep <160704566+Abhinavd2004@users.noreply.github.com>
2024-12-01 19:54:11 +0530

    Update Booking_And_Stuff.c

--- Bookmyc/Booking_And_Stuff.c
+++ Bookmyc/Booking_And_Stuff.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+void beverage();
 int main(){
     char seatschart[40][5]={ "a1","a2","a3","a4","a5","a6","a7","a8","a9","a10","b1","b2","b3","b4","b5","b6","b7","b8","b9","b10","c1","c2","c3","c4","c5","c6","c7","c8","c9","c10","d1","d2","d3","d4","d5","d6","d7","d8","d9","d10"};
     int m;
@@ -34,12 +35,12 @@ int main(){
         if(m==4){
             break;
         }
+        return 0;
     }
+    beverage();
+    return 0;
     
 }
-#include <stdio.h>
-#include <string.h>
-
 void beverage() {
     int sop = 0;
     int i;
